319cf90bfe188f2447d6dd058024e73e9262ca07
Your Name <ealanraj1472@gmail.com>
2024-06-15 19:04:54 +0530

    local global

--- demo.py
+++ demo.py
@@ -5,3 +5,35 @@ sq(2)
 
 cube = lambda y : y**3
 cube(2)
+
+add = lambda x,y : x+y
+
+#Global variable 
+#local variable
+
+a = 1
+
+# Uses global because there is no local 'a'
+def f():
+	print('Inside f() : ', a)
+
+# Variable 'a' is redefined as a local
+def g():
+	a = 2
+	print('Inside g() : ', a)
+
+# Uses global keyword to modify global 'a'
+def h():
+	global a
+	a = 3
+	print('Inside h() : ', a)
+
+
+# Global scope
+print('global : ', a)
+f()
+print('global : ', a)
+g()
+print('global : ', a)
+h()
+print('global : ', a)
